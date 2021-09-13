a3057891c8adfd7a76048a4d16a0759d928a3fa8
pysachin <71167466+pysachin@users.noreply.github.com>
2021-09-13 17:44:23 +0530

    added API gateway using ocelot

--- src/shopping-cart/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/shopping-cart/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
-﻿using Basket.API.Entities;
+﻿using AutoMapper;
+using Basket.API.Entities;
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using EventBus.Messages.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,11 +19,15 @@ namespace Basket.API.Controllers
     {
         private readonly IBasketRepository _repository;
         private readonly DiscountGrpcService _discountGrpcService;
+        private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IMapper mapper, IPublishEndpoint publishEndpoint)
         {
             _repository = repository;
             _discountGrpcService = discountGrpcService;
+            _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -52,5 +59,31 @@ namespace Basket.API.Controllers
             await _repository.DeleteBasket(userName);
             return Ok();
         }
+
+        [Route("[action]")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            // get existing basket with total price
+            // Create basketCheckoutEvent -- TotalPrice on basekCheckout eventMessage
+            // send checkout event to rabbitmq
+            // remove the basket
+
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if(basket == null)
+            {
+                return BadRequest();
+            }
+
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMessage);            
+            await _repository.DeleteBasket(basketCheckout.UserName);
+
+            return Accepted();
+        }
+
     }
 }
--- src/shopping-cart/Services/Basket/Basket.API/Startup.cs
+++ src/shopping-cart/Services/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -40,6 +41,17 @@ namespace Basket.API
                 );
 
             services.AddScoped<DiscountGrpcService>();
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx,cfg) =>
+                {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+
+            services.AddMassTransitHostedService();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
